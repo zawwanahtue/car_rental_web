8677fd782bf4e0dcd57766b3cec614aee963c48d
zawwanahtue <yaza9036@gmail.com>
2025-05-11 17:26:22 +0630

    first api testing

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/root', function(){
+    return response()->json(['hello htet hlaing swan']);
+});
